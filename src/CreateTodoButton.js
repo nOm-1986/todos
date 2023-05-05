4505478cf0c1be127043e75f03d999e20937db1a
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-05 17:34:18 -0500

    Events on React

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -2,15 +2,19 @@ import React from 'react';
 import './CreateTodoButton.css';
 
 
-function CreateTodoButton(props) {
-  const onClickButton = (msg) => {
-    alert(msg);
+function CreateTodoButton() {
+
+  const onClickButton = (event) => {
+    console.log(event);
+    console.log(event.target);
+    console.log(event.target.className);
   }
+
   return (
     <button 
     className="CreateTodoButton"
     //onClick={() => { console.log('Tu primer evento con React') }}
-    onClick={() => {onClickButton('trululu, un mensaje')}}
+    onClick={ onClickButton }
     >
         + 
     </button>
@@ -18,3 +22,4 @@ function CreateTodoButton(props) {
 }
 
 export { CreateTodoButton };
+
